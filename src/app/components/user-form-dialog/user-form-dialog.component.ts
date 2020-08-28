054548ae089224559a560809ba1b0721884dc585
aman-jalan <aman.jalan@meltwater.com>
2020-08-29 00:19:16 +0530

    bug fixes

--- src/app/components/user-form-dialog/user-form-dialog.component.ts
+++ src/app/components/user-form-dialog/user-form-dialog.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UserService } from 'src/app/user-manager/user.service';
-import { resolve } from 'url';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
@@ -17,6 +16,7 @@ export class UserFormDialogComponent implements OnInit {
   userForm: FormGroup;
 
   ngOnInit() {
+    console.log("user: ", this.data);
     this.userForm = new FormGroup({
       email: new FormControl(null, Validators.required),
       password: new FormControl(null, Validators.required),
@@ -25,21 +25,33 @@ export class UserFormDialogComponent implements OnInit {
       age: new FormControl(null, Validators.required),
       hobbies: new FormControl(null, Validators.required),
     });
+
+    if (this.data.mode === "Edit") {
+      this.userForm.setValue({
+        email: this.data.user.email,
+        password: this.data.user.password,
+        firstName: this.data.user.firstName,
+        lastName: this.data.user.lastName,
+        age: this.data.user.age,
+        hobbies: this.data.user.hobbies
+      });
+      this.userForm.controls['email'].disable();
+    }
   }
 
   submit() {
     if (this.userForm.valid)
       if (this.data.mode === 'Add') {
         this.userService.addUser(this.userForm.value).then(() => {
-          return (true);
+          this.dialogRef.close(true);
         }).catch(() => {
           return (false);
         });
       } else {
         this.userService.updateUser(this.userForm.value).then(() => {
-          return (true);
+          this.dialogRef.close(true);
         }).catch(() => {
-          return (false);
+          this.dialogRef.close(false);
         });
       }
     else
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: 'localhost:8081'
+  url: 'http://localhost:3000'
 };
 
 /*
